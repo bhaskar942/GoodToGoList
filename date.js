d5f337198c6664f60a1e91e8cfd1c00fe04ad3d4
Bhaskar Arya <64805871+bhaskar942@users.noreply.github.com>
2021-04-27 22:09:28 +0530

    Update date.js

--- date.js
+++ date.js
@@ -1,18 +1,18 @@
 module.exports.getDate=getDate;
 
-function getDate(){
-var today=new Date();
-var options={day: "numeric",month: "long",weekday: "long"}
-var day=today.toLocaleString("en-US",options);
-return day;
-}
-//same above function can be written as
-//getDate=function(){
+//function getDate(){
 //var today=new Date();
 //var options={day: "numeric",month: "long",weekday: "long"}
 //var day=today.toLocaleString("en-US",options);
 //return day;
 //}
+//same above function can be written as
+getDate=function(){
+var today=new Date();
+var options={day: "numeric",month: "long",weekday: "long"}
+var day=today.toLocaleString("en-US",options);
+return day;
+}
 //shorter and smater way..we can use exports.gatDay instead of module.exports.getDay and then just bind it to a corrosponding anonymous function
 exports.getDay=function(){
   var today=new Date();
